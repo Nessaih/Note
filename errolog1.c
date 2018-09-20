9e078c3d692ac43308280261072cc24fb6874b9d
Nessaih <37034973+Nessaih@users.noreply.github.com>
2018-09-20 15:48:39 +0800

    Create errolog1.c

---
+++ errolog1.c
@@ -0,0 +1,49 @@
+typedef struct {
+	__IO uint32_t DATA;
+#define PIN0    0
+#define PIN1    1
+#define PIN2    2
+#define PIN3    3
+#define PIN4    4
+#define PIN5    5
+#define PIN6    6
+#define PIN7    7
+#define PIN8    8
+#define PIN9    9
+#define PIN10   10
+#define PIN11   11
+#define PIN12   12
+#define PIN13   13
+#define PIN14   14
+#define PIN15   15
+
+	__IO uint32_t DIR;					    //0 输入	1 输出
+
+	__IO uint32_t INTLVLTRG;				//Interrupt Level Trigger  1 电平触发中断	0 边沿触发中断
+
+	__IO uint32_t INTBE;					//Both Edge，当INTLVLTRG设为边沿触发中断时，此位置1表示上升沿和下降沿都触发中断，置0时触发边沿由INTRISEEN选择
+
+	__IO uint32_t INTRISEEN;				//Interrupt Rise Edge Enable   1 上升沿/高电平触发中断	0 下降沿/低电平触发中断
+
+	__IO uint32_t INTEN;					//1 中断使能	0 中断禁止
+
+	__IO uint32_t INTRAWSTAT;			    //中断检测单元是否检测到了触发中断的条件 1 检测到了中断触发条件	0 没有检测到中断触发条件
+
+	__IO uint32_t INTSTAT;				    //INTSTAT.PIN0 = INTRAWSTAT.PIN0 & INTEN.PIN0
+
+	__IO uint32_t INTCLR;				    //写1清除中断标志，只对边沿触发中断有用
+} GPIO_TypeDef;
+
+
+
+
+已知有以上结构体，定义如下宏
+#define    LED_IO_ADDR    (GPIOA + PIN5)
+GPIOA = 0X5000 1000
+PIN5 = 0X0000 0005
+
+却发现
+LED_IO_ADDR = 0X5000 10B4
+并不是希望获得的LED_IO_ADDR =0X5000 1005
+
+原因：GPIOA类型为(GPIO_TypeDef *)，是一个指针，每次+1地址会+36(GPIO_TypeDef类型占36个字节)
